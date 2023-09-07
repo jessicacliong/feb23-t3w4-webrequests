114ec6bf10bfdddcf767920be0efd4e78f654dc2
jessicacliong <14395@coderacademy.edu.au>
2023-09-07 19:57:42 +1000

    feat: add API data image to screen

--- promiseRequest.js
+++ promiseRequest.js
@@ -10,10 +10,19 @@ function getRandomPokemon(){
 	let result = fetch(API_BASE_URL + ID).then((response) => {
 		console.log("Response is: " + JSON.stringify(response, null, 4));
 
+
+
 		return response.json();
 	})
 	.then(data => {
 		console.log("Data is: " + JSON.stringify(data, null, 4));
+
+    console.log("Data retrieved for: " + data.name);
+    console.log("Image is: " + data.sprites.other["official-artwork"].front_default);
+
+    let pkmImage = document.getElementById("pokemonImage");
+    pkmImage.src = data.sprites.other["official-artwork"].front_default;
+
 
 		return data;
 	}).catch(error => {
